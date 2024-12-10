2313895c25c30ea28ab12c0f2b0a5dd58696c4df
Sujit Wagh <sujitwagh1233@gmail.com>
2024-12-11 00:13:26 +0530

    added public user Routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -13,27 +13,6 @@ router.post("/refresh-token", refreshAccessToken); // Refresh access token
 router.post("/forgot-password", forgotPassword); // Forgot password request
 router.post("/reset-password", resetPassword); // Reset password
 router.get('/verify-email', verifyEmail);
-// Protected Routes (Requires Authentication)
-router.get("/profile", authenticateUser, (req, res) => {
-  res.json({ user: req.user }); // View logged-in user's profile
-});
 
-// Admin-only Route (Example: Getting all instructors)
-router.get(
-  "/instructors",
-  authenticateUser,
-  authorizeRoles("admin"), // Only admin can access
-  async (req, res, next) => {
-    try {
-      const instructors = await User.find({ role: "instructor" }, { password: 0, refreshTokens: 0 });
-      res.status(200).json({
-        message: "Instructors retrieved successfully",
-        instructors,
-      });
-    } catch (error) {
-      next(new ApiError(500, "Error retrieving instructors"));
-    }
-  }
-);
 
 export default router;
